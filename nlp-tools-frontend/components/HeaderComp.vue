07aee9276538f9069a54943c54abaf78c748af02
Adriano Ferreira <adriano.13lp@gmail.com>
2024-10-16 12:09:06 -0300

    style: link para o repositório adicionado em HeaderComp

--- nlp-tools-frontend/components/HeaderComp.vue
+++ nlp-tools-frontend/components/HeaderComp.vue
@@ -1,8 +1,23 @@
 <template>
   <header>
     <div class="flex items-center justify-between">
-      <h1 class="text-3xl font-bold"><span
-          class="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">NLP</span> Tools</h1>
+      <h1 class="text-3xl font-bold">
+        <span
+          class="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent"
+          >NLP</span
+        >
+        Tools
+      </h1>
+      <p>
+        <a
+          href="https://github.com/lopes-adriano/nlp-tools"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:underline"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   </header>
 </template>
